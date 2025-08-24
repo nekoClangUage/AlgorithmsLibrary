f5e31a0be1e7dee5f47ddf62531ce09a588130bd
alexandra <alexandr36wzfbntfpdcifq@gmail.com>
2025-08-24 14:35:47 +0300

    Add search.py with future methods

---
+++ src/search.py
@@ -0,0 +1,10 @@
+class Search:
+    @staticmethod
+    def bruteforce_Search(array, target_elem):
+        pass
+
+    @staticmethod
+    def binary_Search(array, target_elem):
+        pass
+    
+    
