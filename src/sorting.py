48506dbf2493d7705cfc4d7168c72223602aa60c
alexandra <alexandr36wzfbntfpdcifq@gmail.com>
2025-08-24 14:31:15 +0300

    Add OddEven with parallel computations and SingleThread implementation. And tests for them.

--- src/sorting.py
+++ src/sorting.py
@@ -1,7 +1,25 @@
+import multiprocessing as mpi 
+
+def swap(array, i, j):
+    """
+        Swap elements in array
+
+        Args:
+            array: Any object that supports iteration, indexing, and whose
+                         elements support comparison operations.
+            i: index element in array.
+            j: index element in array.
+    """
+    temp = array[j]
+
+    array[j] = array[i]
+    array[i] = temp
+
 class BubbleSort:
     @staticmethod
     def simpleBubble(array):
-        """Sorts a collection using the bubble sort algorithm.
+        """
+        Sorts a collection using the bubble sort algorithm.
 
         Args:
             array: Any object that supports iteration, indexing, and whose
@@ -20,5 +38,67 @@ class BubbleSort:
 
                 j += 1
             i += 1
-    def oddEvenBubble(array):
-        pass
+
+    @staticmethod
+    def oddEvenBubble(array, num_processes=2):
+        """
+        Sorts a collection using a parallelized odd-even sort.
+
+        Args:
+            num_processes: The number of processes to use for sorting.
+            array: Any object that supports iteration, indexing, and whose
+                         elements support comparison operations.            
+        """
+        arr_len = len(array)
+        if arr_len < 2:
+            return
+
+        if num_processes < 1 or num_processes > 20:
+            num_processes = 1
+
+        def pass_worker(shared_list, start_index, step):
+            for j in range(start_index, arr_len, step):
+                if shared_list[j] < shared_list[j - 1]:
+                    swap(shared_list, j - 1, j)
+
+        with mpi.Manager() as manager:
+            shared_list = manager.list(array)
+            processes = []
+            step = 2 * num_processes
+
+            for i in range(arr_len):
+                processes.clear()
+                start_offset = 1 if i % 2 == 0 else 2
+
+                for j in range(num_processes):
+                    start_index = start_offset + 2 * j
+                    p = mpi.Process(target=pass_worker, args=(shared_list, start_index, step))
+                    processes.append(p)
+                    p.start()
+
+                for p in processes:
+                    p.join()
+
+            for i in range(arr_len):
+                array[i] = shared_list[i]
+
+    @staticmethod
+    def oddEvenBubbleSingleThread(array):
+        """
+        Sorts a collection using the bubble sort algorithm (single thread; no parallelism).
+        Args:
+            array: Any object that supports iteration, indexing, and whose
+                        elements support comparison operations.
+        """
+        i = 0
+        arr_len = len(array)
+        while(i < arr_len):
+            if(i % 2 == 0):        
+                for j in range(2, arr_len, 2):
+                    if(array[j] < array[j - 1]):
+                        swap(array, j - 1, j)
+            else:
+                for j in range(1, arr_len, 2):
+                    if(array[j] < array[j - 1]):
+                        swap(array, j - 1, j)
+            i += 1
--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -1,6 +1,8 @@
 import pytest
 from src.sorting import BubbleSort
 
+# ------------------------------------------------------
+
 @pytest.mark.parametrize("input_array, expected_array", [
     ([3, 2, 1], [1, 2, 3]),
     ([5, 1, 4, 2, 8], [1, 2, 4, 5, 8]),
@@ -9,7 +11,42 @@ from src.sorting import BubbleSort
     ([2, 2, 2], [2, 2, 2]),
     ([1, 2, 3, 4, 5], [1, 2, 3, 4, 5])
 ])
-def test_bubble_sort(input_array, expected_array):
+def test_simpleBubble_sort(input_array, expected_array):
     sorter = BubbleSort()
     sorter.simpleBubble(input_array)
     assert input_array == expected_array
+
+# ------------------------------------------------------
+
+@pytest.mark.parametrize("num_processes", [2, 4, 6, 8, 10, 12, 14, 16, 18, 20])
+@pytest.mark.parametrize("input_array, expected_array", [
+    ([3, 2, 1], [1, 2, 3]),
+    ([5, 1, 4, 2, 8], [1, 2, 4, 5, 8]),
+    ([], []),
+    ([1], [1]),
+    ([2, 2, 2], [2, 2, 2]),
+    ([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]),
+    (list(range(100, 0, -1)), list(range(1, 101)))
+])
+def test_oddevenBubble_sort(input_array, expected_array, num_processes):
+    array_to_sort = input_array.copy()
+    sorter = BubbleSort()
+    sorter.oddEvenBubble(array_to_sort, num_processes=num_processes)
+    assert array_to_sort == expected_array
+
+# ------------------------------------------------------
+
+@pytest.mark.parametrize("input_array, expected_array", [
+    ([3, 2, 1], [1, 2, 3]),
+    ([5, 1, 4, 2, 8], [1, 2, 4, 5, 8]),
+    ([], []),
+    ([1], [1]),
+    ([2, 2, 2], [2, 2, 2]),
+    ([1, 2, 3, 4, 5], [1, 2, 3, 4, 5])
+])
+def test_oddevenBubbleSingleThread_sort(input_array, expected_array):
+    sorter = BubbleSort()
+    sorter.oddEvenBubbleSingleThread(input_array)
+    assert input_array == expected_array
+
+# ------------------------------------------------------
